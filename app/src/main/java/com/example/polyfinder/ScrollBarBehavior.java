2848a5ac0f247e67e964298b2ddd7ceac0d3ae0f
mishokU <34416536+mishokU@users.noreply.github.com>
2019-05-15 20:08:36 +0300

    Add more layouts

---
+++ app/src/main/java/com/example/polyfinder/ScrollBarBehavior.java
@@ -0,0 +1,43 @@
+package com.example.polyfinder;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.ViewCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ScrollBarBehavior extends FloatingActionButton.Behavior {
+
+    public ScrollBarBehavior(Context context, AttributeSet attributeSet){
+        super(context,attributeSet);
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+
+        if(dyConsumed > 0 && child.getVisibility() == View.VISIBLE){
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                @SuppressLint("RestrictedApi")
+                @Override
+                public void onHidden(FloatingActionButton fab) {
+                    super.onShown(fab);
+                    fab.setVisibility(View.INVISIBLE);
+                }
+            });
+
+        } else if(dyConsumed < 0 && child.getVisibility() == View.INVISIBLE){
+            child.show();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, @NonNull View target, int axes, int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+}
