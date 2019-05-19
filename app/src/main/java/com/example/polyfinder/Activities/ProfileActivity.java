adf02f692ab9301389edf989ca35081b3b524741
vlad <gilbert.cook111@gmail.com>
2019-05-19 20:27:11 +0300

    profile data

--- app/src/main/java/com/example/polyfinder/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/polyfinder/Activities/ProfileActivity.java
@@ -28,23 +28,38 @@ import com.example.polyfinder.Items.Requests;
 import com.example.polyfinder.R;
 import com.google.android.material.appbar.AppBarLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 import jp.wasabeef.recyclerview.animators.SlideInLeftAnimator;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar) public Toolbar mToolbar;
-    @BindView(R.id.recyclerview) public RecyclerView mRecyclerView;
-    @BindView(R.id.user_name) public TextView mUserName;
-    @BindView(R.id.telephone) public TextView mUserPhoneNumber;
+    @BindView(R.id.toolbar) public Toolbar toolbar;
+    @BindView(R.id.recyclerview) public RecyclerView recyclerView;
+    @BindView(R.id.user_name) public TextView userName;
+    @BindView(R.id.telephone) public TextView userPhoneNumber;
+    @BindView(R.id.profile_image) public CircleImageView profileImage;
 
     private ArrayList<Requests> mRequestItems = new ArrayList<>();
     private RecyclerView.LayoutManager mLayoutManager;
     private RecyclerView.Adapter mAdapter;
+
+    private DatabaseReference reference;
+    private FirebaseUser currentUser;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,17 +67,53 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.profile_activity);
         ButterKnife.bind(this);
 
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        assert currentUser != null;
+        String user_id = currentUser.getUid();
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        loadData();
+
         setUpToolbar();
         setUpAdapter();
+    }
+
+    private void loadData() {
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String txt_name = dataSnapshot.child("username").getValue().toString();
+                String txt_email = dataSnapshot.child("email").getValue().toString();
+                String image = dataSnapshot.child("imageUrl").getValue().toString();
+                if(dataSnapshot.hasChild("phone")){
+                    String phone = dataSnapshot.child("phone").getValue().toString();
+                    userPhoneNumber.setText(phone);
+                }
+
+                userName.setText(txt_name);
+                //email.setText(txt_email);
+                Picasso.get().load(image).placeholder(R.mipmap.ic_launcher).into(profileImage);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void setUpAdapter() {
         mLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
         mAdapter = new MainTypeRequestAdapter(mRequestItems);
 
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(mAdapter);
 
         setUpTouchListener();
     }
@@ -80,11 +131,11 @@ public class ProfileActivity extends AppCompatActivity {
                 mRequestItems.remove(((MainTypeRequestAdapter)mAdapter).getRequestAt(viewHolder.getAdapterPosition()));
                 mAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
             }
-        }).attachToRecyclerView(mRecyclerView);
+        }).attachToRecyclerView(recyclerView);
     }
 
     private void setUpToolbar() {
-        setSupportActionBar(mToolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Back to main tape");
     }
