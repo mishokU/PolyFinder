59c9459db759762b9aa396f3abe0176dea646320
mishokU <34416536+mishokU@users.noreply.github.com>
2019-05-23 12:36:26 +0300

    Add type to requests with animation and bad colors

--- app/src/main/java/com/example/polyfinder/Fragments/BottomFoundRequest.java
+++ app/src/main/java/com/example/polyfinder/Fragments/BottomFoundRequest.java
@@ -1,19 +1,25 @@
 package com.example.polyfinder.Fragments;
 
+import android.animation.Animator;
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.polyfinder.R;
@@ -26,10 +32,16 @@ public class BottomFoundRequest extends Fragment{
     private EditText mTitle;
     private EditText mDescription;
     private Button mPublish;
+    private Button mType;
     private ImageView mCloseFragment;
     private Transmitter transmitter;
+    private RelativeLayout mTypePlace;
+    private androidx.constraintlayout.widget.ConstraintLayout mTextPlace;
     //private Type type;
     //private Image image;
+
+    private boolean isOpen = true;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.found_fragment, container, false);
@@ -47,6 +59,86 @@ public class BottomFoundRequest extends Fragment{
                 transmitter.OnCloseSend(true);
             }
         });
+        mType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTypeMenu();
+            }
+        });
+    }
+
+    private void openTypeMenu() {
+        if(isOpen) {
+            int x = (int) mType.getX() + mType.getWidth() / 2;
+            int y = (int) mType.getY() + mType.getHeight() / 2;
+
+            int startRadius = 0;
+            int endRadius = mTextPlace.getWidth();
+
+            Animator anim = ViewAnimationUtils.createCircularReveal(mTypePlace, x, y, startRadius, endRadius);
+            anim.setDuration(1000);
+            mTypePlace.setVisibility(View.VISIBLE);
+            anim.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mTitle.setVisibility(View.INVISIBLE);
+                    mDescription.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+
+            anim.start();
+
+            isOpen = false;
+        } else {
+
+            int x = (int) mType.getX() + mType.getWidth() / 2;
+            int y = (int) mType.getY() + mType.getHeight() / 2;
+
+            int startRadius = mTextPlace.getWidth();
+            int endRadius = 0;
+
+            Animator anim = ViewAnimationUtils.createCircularReveal(mTypePlace, x, y, startRadius, endRadius);
+            anim.setDuration(1000);
+            anim.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                    mTitle.setVisibility(View.VISIBLE);
+                    mDescription.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    mTypePlace.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+            anim.start();
+            isOpen = true;
+        }
     }
 
     private void findAllViews() {
@@ -54,6 +146,9 @@ public class BottomFoundRequest extends Fragment{
         mDescription = view.findViewById(R.id.description);
         mPublish = view.findViewById(R.id.publish);
         mCloseFragment = view.findViewById(R.id.close_fragment);
+        mType = view.findViewById(R.id.type);
+        mTextPlace = view.findViewById(R.id.text_place);
+        mTypePlace = view.findViewById(R.id.type_place);
     }
 
     @Override
--- app/src/main/java/com/example/polyfinder/Fragments/BottomLostRequest.java
+++ app/src/main/java/com/example/polyfinder/Fragments/BottomLostRequest.java
@@ -1,15 +1,20 @@
 package com.example.polyfinder.Fragments;
 
+import android.animation.Animator;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.polyfinder.R;
@@ -21,8 +26,16 @@ public class BottomLostRequest extends Fragment {
     private View view;
     private RelativeLayout mainRelative;
     private TextView focus;
+    private EditText mTitle;
+    private Button mPublish;
+    private EditText mDescription;
     private Transmitter transmitter;
     private ImageView mCloseFragment;
+    private Button mType;
+    private RelativeLayout mTypePlace;
+    private ConstraintLayout mTextPlace;
+
+    private boolean isOpen = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -41,12 +54,98 @@ public class BottomLostRequest extends Fragment {
                 transmitter.OnCloseSend(true);
             }
         });
+        mType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openTypeMenu();
+            }
+        });
     }
 
     private void findViews() {
         mCloseFragment = view.findViewById(R.id.close_fragment);
+        mTitle = view.findViewById(R.id.title);
+        mDescription = view.findViewById(R.id.description);
+        mPublish = view.findViewById(R.id.publish);
+        mCloseFragment = view.findViewById(R.id.close_fragment);
+        mType = view.findViewById(R.id.type);
+        mTextPlace = view.findViewById(R.id.text_place);
+        mTypePlace = view.findViewById(R.id.type_place);
     }
 
+    private void openTypeMenu() {
+        if(isOpen) {
+            int x = (int) mType.getX() + mType.getWidth() / 2;
+            int y = (int) mType.getY() + mType.getHeight() / 2;
+
+            int startRadius = 0;
+            int endRadius = mTextPlace.getWidth();
+
+            Animator anim = ViewAnimationUtils.createCircularReveal(mTypePlace, x, y, startRadius, endRadius);
+            anim.setDuration(1000);
+            mTypePlace.setVisibility(View.VISIBLE);
+            anim.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mTitle.setVisibility(View.INVISIBLE);
+                    mDescription.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+
+            anim.start();
+
+            isOpen = false;
+        } else {
+
+            int x = (int) mType.getX() + mType.getWidth() / 2;
+            int y = (int) mType.getY() + mType.getHeight() / 2;
+
+            int startRadius = mTextPlace.getWidth();
+            int endRadius = 0;
+
+            Animator anim = ViewAnimationUtils.createCircularReveal(mTypePlace, x, y, startRadius, endRadius);
+            anim.setDuration(1000);
+            anim.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                    mTitle.setVisibility(View.VISIBLE);
+                    mDescription.setVisibility(View.VISIBLE);
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animator) {
+                    mTypePlace.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animator) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animator) {
+
+                }
+            });
+            anim.start();
+            isOpen = true;
+        }
+    }
 
 
     @Override
